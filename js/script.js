ed9f4b9b0aacc32ba33cfbe1560190e90940d7c7
OlehKashchuk <oleh.kashchuk@gmail.com>
2022-02-08 21:47:27 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let obj = [];
+
+obj[0] = prompt("what is your name?", "");
+obj[1] = prompt("what is your surname?", "");
+obj[2] = prompt("how old are you?", "");
+
+document.write(obj);
