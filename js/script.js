9f71c94b6b74a7f39f93b55b18fa71f814127619
OlehKashchuk <oleh.kashchuk@gmail.com>
2022-02-09 23:18:46 +0200

    Create practical task #2

--- js/script.js
+++ js/script.js
@@ -4,11 +4,6 @@
 
 const numberOfFilms = +prompt("How many films do you already saw?", "");
 
-    let a = prompt("One of the last watched movies?", ""),
-        b = prompt("How much will you rate the film?", ""),
-        c = prompt("How much will you rate the film?", ""),
-        d = prompt("How much will you rate the film?", "");
-
 let personalMovieDB = {
     'count': numberOfFilms,
     'movie': {},
@@ -16,6 +11,28 @@ let personalMovieDB = {
     'genres': [],
     'privat': false
 };
-personalMovieDB.movie[a] = b;
-personalMovieDB.movie[c] = d;
+
+let countOfQuestions = 0;
+
+while (countOfQuestions < 2) {
+    let a = prompt("One of the last watched movies?", ""),
+        b = prompt("How much will you rate the film?", "");
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movie[a] = b;
+        countOfQuestions++;
+    } else {
+        console.log('error');
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert("very few movies watched");
+} else if (personalMovieDB.count >=10 && personalMovieDB.count < 30) {
+    alert("you are a classic spectator");
+} else if(personalMovieDB.count >= 30){
+    alert('you are a movie buff');
+}else {
+    alert('error');
+}
+
 console.log(personalMovieDB);
